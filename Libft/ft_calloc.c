06fa2caf65b5530f1dab5c62ed82056722ff6d53
steffli <stephan-liu@hotmail.de>
2025-03-11 22:48:46 +0100

    added calloc and strdup

--- Libft/ft_calloc.c
+++ Libft/ft_calloc.c
@@ -1,42 +1,31 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*   ft_calloc.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: stliu <stliu@student.42heilbronn.de>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/03/11 22:13:36 by stliu             #+#    #+#             */
-/*   Updated: 2025/03/11 22:13:36 by stliu            ###   ########.fr       */
+/*   Created: 2025/03/11 22:27:51 by stliu             #+#    #+#             */
+/*   Updated: 2025/03/11 22:27:51 by stliu            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-#include <stdio.h>
-#include <stdlib.h>
-char *ft_strdup(const char *s)
-{
-	int i;
-	char *ptr;
-	
-	i = 0;
-	while(s[i] != '\0')
-		i++;
-	//strlen
-	ptr = malloc((i + 1) * sizeof(char));
-	if (ptr == NULL)
-		return (NULL);
-	i = 0;
-	while(s[i])
-	{
-		ptr[i] = s[i];
-		i++;
-	}
-	ptr[i] = '\0';
 
+void *ft_calloc(size_t nmemb, size_t size)
+{
+	void *ptr;
+	size_t i;
+
+	i = 0;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if(ptr == NULL)
+		return NULL;
+	while(i < nmemb * size)
+		{
+			((char *)ptr)[i] = '\0';
+			i++;
+		}
+	
 	return (ptr);
 }
-
-// int main ()
-// {
-// 	char *s1  = "String";
-// 	char *s2  = ft_strdup(s1);
-// 	printf("%s", s2);
-// }
